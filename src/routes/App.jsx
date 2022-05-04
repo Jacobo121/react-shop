d72b3c19130a39109b486ec855292b16baced874
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-05-04 17:39:11 -0500

    organizando las rutas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,21 @@
+/* imports */
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+/*------------- pages --------------*/
 import Layout from '../containers/Layout';
-import Login from '../pages/Login.jsx'
-import RecoveryPasswordEmail from '../containers/RecoveryPasswordEmail';
 import Home from '../pages/Home.jsx';
+import Login from '../pages/Login.jsx'
+import RecoveryPassword from '../pages/RecoveryPasswordEmail';
+import RecoveryPasswordEmail from '../containers/RecoveryPasswordEmail';
+import NewPassword from '../pages/NewPassword.jsx';
+import MyAccount from '../pages/MyAccount.jsx';
+import CreateAccount from '../pages/CreateAccount.jsx';
+import Checkout from '../pages/Checkout.jsx';
+import Orders from '../pages/Orders.jsx';
 import NotFound from '../pages/NotFound';
+
+/* -----------Styles --------------- */
 import '../styles/global.css';
 
 const App = () => {
@@ -14,7 +25,13 @@ const App = () => {
                 <Routes>
                     <Route path='/' element={<Home />}/>
                     <Route path='/login' element={<Login />}/>
-                    <Route path='/recovery-password' element={<RecoveryPasswordEmail />}/>
+                    <Route path='/recovery-password' element={<RecoveryPassword />}/>
+                    <Route path='/recovery-password-email' element={<RecoveryPasswordEmail />}/>
+                    <Route path='/new-password' element={<NewPassword />}/>
+                    <Route path='/account' element={<MyAccount />}/>
+                    <Route path='/signup' element={<CreateAccount />}/>
+                    <Route path='/checkout' element={<Checkout />}/>
+                    <Route path='/orders' element={<Orders />}/>
                     <Route path='*' element={<NotFound />} />
                 </Routes>
             </Layout>
