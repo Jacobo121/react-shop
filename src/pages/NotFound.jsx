5aa50e7f86966f37f7257a271d3219b3f6c229b5
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-05-11 16:30:08 -0500

    indentando todos los archivos

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import '../styles/NotFound.scss';
+import React from "react";
+import "../styles/NotFound.scss";
 
 const NotFound = () => {
-    return (
-        <div className='NotFound'>
-            <h1 className='title'><span className='ups'>Ups..</span> <br />Parece que encontraste un error</h1>
-            <div className='animation'>
-                <div class="ball"></div>
-                <div class="shadow"></div>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="NotFound">
+      <h1 className="title">
+        <span className="ups">Ups..</span> <br />
+        Parece que encontraste un error
+      </h1>
+      <div className="animation">
+        <div class="ball"></div>
+        <div class="shadow"></div>
+      </div>
+    </div>
+  );
+};
 
 export default NotFound;
