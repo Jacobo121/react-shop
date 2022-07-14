01925d1e6f95092e2d6a491203ab026b6f5a7172
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-07-14 13:30:00 -0500

    clase-17

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/ProductItem.scss";
 
-import addToCart from '@icons/bt_add_to_cart.svg'
+import addToCart from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = () => {
+ 
+  const [cart, setCart] = useState('');
+
+  const handleClick = () => {
+    setCart('Hola mundo');
+  }
+  
   return (
     <div className="ProductItem">
       <img
@@ -15,9 +22,10 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleClick} >
           <img src={addToCart} alt="" />
         </figure>
+        {cart}
       </div>
     </div>
   );
